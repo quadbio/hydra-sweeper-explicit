129aebf34f15218164b155fd8bef419133439916
Marius Lange <marius.lange@bsse.ethz.ch>
2026-01-29 15:40:55 +0100

    Fix pre-commit issues

--- src/hydra_sweeper_explicit/_searchpath.py
+++ src/hydra_sweeper_explicit/_searchpath.py
@@ -1,15 +1,12 @@
 """SearchPath plugin to add the sweeper's config to Hydra's search path."""
 
-from hydra.core.global_hydra import GlobalHydra
+from hydra.core.config_search_path import ConfigSearchPath
 from hydra.plugins.search_path_plugin import SearchPathPlugin
 
 
 class ExplicitSweeperSearchPathPlugin(SearchPathPlugin):
     """Add the explicit sweeper config directory to Hydra's search path."""
 
-    def manipulate_search_path(self, search_path: "ConfigSearchPath") -> None:
+    def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
         """Add the package's config directory to the search path."""
-        from hydra.core.plugins import Plugins
-        from hydra.plugins.search_path_plugin import SearchPathPlugin
-
         search_path.append(provider="hydra-sweeper-explicit", path="pkg://hydra_sweeper_explicit.conf")
